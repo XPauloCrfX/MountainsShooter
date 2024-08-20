94ff9e1a650535037945e6c653136a7428f465bc
XPauloCrfX <paulo4545cesar@gmail.com>
2024-08-19 21:38:07 -0300

    Adding the assets and updating menu

--- code/game.py
+++ code/game.py
@@ -1,5 +1,6 @@
 import pygame
 from code.menu import Menu
+from const import WIN_HEIGHT, WIN_WIDTH
 
 
 #!/usr/bin/python
@@ -8,16 +9,14 @@ from code.menu import Menu
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
+
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()
-            #         quit()
+
 
